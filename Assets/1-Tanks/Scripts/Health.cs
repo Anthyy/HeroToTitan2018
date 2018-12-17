adc761be2e36d95adb6ffe5337a45bfad68a433c
Anthyy <42423921+Anthyy@users.noreply.github.com>
2018-12-17 16:02:33 +1100

    Initial commit

---
+++ Assets/1-Tanks/Scripts/Health.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+using UnityEngine.UI; 
+
+namespace Tanks
+{
+    public class Health : MonoBehaviour
+    {
+
+        public float maxHealth = 100f;
+        public UnityEvent onDeath;
+        [Header("UI")]
+        public Transform healthSliderParent;
+        public GameObject healthSliderPrefab;
+        public Vector3 offset = new Vector2(0, 1f);
+
+        [HideInInspector] public Vector3 lastHitpoint; // The opposite of [SerializeField]. You want to hide it but still want to be able to reference it
+
+
+        private float currentHealth = 100f;
+        private Slider healthSlider;
+
+        #region Unity Functions
+        // Use this for initialization
+        void Start()
+        {
+            SpawnUI();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateUI();
+        }
+
+        private void OnDestroy()
+        {
+            // If health slider exists
+            if (healthSlider)
+            {
+                // Destroy HealthSlider UI
+                Destroy(healthSlider.gameObject);
+            }
+        }
+        #endregion
+
+        #region Custom Functions
+        private void SpawnUI()
+        {
+            // Create instance of UI and attach as child to UI parent
+            GameObject clone = Instantiate(healthSliderPrefab, healthSliderParent);
+            // Name health, e.g. "Tank_1_Health"
+            clone.name = name + "_Health";
+            // Get Slider component from clone
+            healthSlider = clone.GetComponent<Slider>();
+        }
+
+        private void UpdateUI()
+        {
+            // Convert world position to screen
+            Vector3 uiPos = Camera.main.WorldToScreenPoint(transform.position + offset);
+            // Update slider position
+            healthSlider.transform.position = uiPos;
+            // Convert health to a 0 - 1 value and update slider value
+            healthSlider.value = currentHealth / maxHealth;
+        }
+
+        private void Dead()
+        {
+            // Invoke all events
+            onDeath.Invoke();
+        }
+
+        public void TakeDamage(float damage, Vector2 hitFrom)
+        {
+            lastHitpoint = hitFrom; // Record last hit position
+            currentHealth -= damage; // Reducing health with damage
+
+            // If health is depleted
+            if(currentHealth <= 0)
+            {
+                // Ya dead
+                Dead();
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/1-Tanks/Scripts/Tank.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; // Allows you to use sliders
+
+namespace Tanks
+{
+    public class Tank : MonoBehaviour
+    {
+        public float speed = 20f; // Travel distance over time
+        public float moveDuration = 2f; // Time allowed for movement
+        [Header("Bullets")]
+        public float bulletSpeed = 10f; // Speed to send bullet traveling
+        public GameObject bulletPrefab; // Original copy of bullet
+        [Header("Explosion")]
+        public float explosionForce = 5f; // Force of explosion
+        public GameObject[] spawnParts; // Parts to spawn when exploding (instantiating each piece to mimic destruction)
+        public Transform[] bodyParts; // Locations to spawn parts
+        [Header("Fuel UI")]
+        public Transform fuelSliderParent; // The parent of the UI Canvas
+        public GameObject fuelSliderPrefab; // The slider prefab to spawn to UI Canvas
+        public Vector2 offset = new Vector2(0, 2f); // Position of the fuel slider
+        [Header("References")]
+        public Transform gun; // Reference to gun for rotating the turret
+        public Transform spawnPoint; // Transform point to spawn the bullet
+        [Header("Components")]
+        public Rigidbody2D rBody; // Reference to rigidbody component
+        public Health health; // Reference to health component
+
+        private float moveTimer = 0f; // Elapsed time of movement (fuel)
+        private Slider fuelSlider; // Reference to newly spawned slider (UI)
+        private bool isPlaying = false; // Is this tank currently playing? (the game is turn-based, like Worms)
+
+        #region Unity Functions
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnDestroy()
+        {
+            
+        }
+
+        private void Reset()
+        {
+            
+        }
+        #endregion
+
+        #region Custom Functions
+        private void SpawnUI()
+        {
+
+        }
+        private void UpdateUI()
+        {
+
+        }
+        private void Move()
+        {
+
+        }
+        private void Shoot()
+        {
+
+        }
+        private void RotateGunToMouse() // Making it so wherever you point the mouse, the turret points in that same direction
+        {
+
+        }
+        private void Explode()
+        {
+            // Loops through all the body parts
+                // Spawns individual pieces to explode
+        }
+        public void Died() 
+        {
+            // Plays death animation
+            // Explodes
+        }
+        #endregion
+    }
+}
