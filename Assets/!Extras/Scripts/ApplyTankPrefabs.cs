5a272edfd804e163a5f16df0e275451c93465ab4
Anthyy <42423921+Anthyy@users.noreply.github.com>
2018-12-19 16:17:47 +1100

    Tank Customisation
    Game actually not finished yet, so ignore previous commit name.

---
+++ Assets/!Extras/Scripts/ApplyTankPrefabs.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tanks.Custom
+{
+    public class ApplyTankPrefabs : MonoBehaviour
+    {
+        public Tank player1, player2;
+        public Transform gun;
+        public int[] partIndexPlayer1 = new int[3];
+        public int[] partIndexPlayer2 = new int[3];
+
+        private void Awake()
+        {           
+            // Get the data from a GameObject that we carried over (via don't destroy)
+            if(GameObject.Find("ButtonHandler") != null)
+            {
+                CreateCharacters handler = GameObject.Find("ButtonHandler").GetComponent<CreateCharacters>();
+
+                partIndexPlayer1 = handler.partIndexPlayer1;
+                partIndexPlayer2 = handler.partIndexPlayer2;
+            }
+        }
+
+        // Use this for initialization
+        
+        void Start()                  
+        {
+            #region Spawn Player 1 Tank Parts                  
+            gun = Instantiate(Resources.Load("Tank_Parts/Body_"+ partIndexPlayer1[0]) as GameObject, player1.transform).transform;           
+            gun = gun.GetChild(0);
+            player1.gun = gun;
+            Instantiate(Resources.Load("Tank_Parts/Turret_" + partIndexPlayer1[1]) as GameObject, gun);
+            player1.spawnPoint = gun.GetChild(0).GetChild(0).GetChild(0).transform;
+            Instantiate(Resources.Load("Tank_Parts/Track_" + partIndexPlayer1[2]) as GameObject, player1.transform);
+            #endregion
+
+            #region Spawn Player 2 Tank Parts
+            gun = Instantiate(Resources.Load("Tank_Parts/Body_" + partIndexPlayer2[0]) as GameObject, player2.transform).transform;
+            gun = gun.GetChild(0);
+            player2.gun = gun;
+            Instantiate(Resources.Load("Tank_Parts/Turret_" + partIndexPlayer2[1]) as GameObject, gun);
+            player2.spawnPoint = gun.GetChild(0).GetChild(0).GetChild(0).transform;
+            Instantiate(Resources.Load("Tank_Parts/Track_" + partIndexPlayer2[2]) as GameObject, player2.transform);
+            #endregion
+        }
+    } 
+}
---
+++ Assets/AwakeAndStart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class example : MonoBehaviour
+{
+    //THESE FUNCTIONS ARE JUST THIS SCRIPT
+    void Awake()
+    {
+        /* Only runs once.
+         - The moment of OUR existance within the world (regardless of how long the world has been around be for we arrive)
+         - Regardless if we are on or off (active or inactive, conscious or unconscious, enabled or disabled)
+         
+        if we are on (active/ conscious/ enabled) we can start our day straight away.
+        if we are off (inactive/ unconscious/ disabled) we can wake up but we have to fully be conscious (enable/ active/ on) to be able to start the day
+         */
+        Debug.Log("I Exist in the world");
+        // Basically, the stuff that's in Awake doesn't need this script to be on in order to run
+    }
+    void Start() // On start of THIS SCRIPT
+    {
+        /*Only runs once
+         - the first frame that THIS script is on (active/ conscious/ enabled)
+         if we are off (inactive/ unconscious/ disabled) we cant do anything as we are pretty much asleep
+
+         */
+        Debug.Log("I am On and Starting my day");
+
+    }
+
+
+}
